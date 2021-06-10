37688977c9d0277766a7681adfc5e775e58ee00f
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-06-10 16:10:35 +0200

    modif

--- components/Modal.js
+++ components/Modal.js
@@ -4,38 +4,48 @@ import { Dialog, Menu, Portal, Transition } from "@headlessui/react";
 function Modal({ isOpen, setIsOpen, currentImage: { src } }) {
 	let imageRef = useRef(null);
 	return (
-		<Transition show={isOpen} as={Fragment}>
-			<Dialog onClose={() => setIsOpen(false)} initialFocus={imageRef}>
-				{/*
-          Use one Transition.Child to apply one transition to the overlay...
-        */}
-				<Transition.Child
-					enter="ease-out duration-300"
-					enterFrom="opacity-0"
-					enterTo="opacity-100"
-					leave="ease-in duration-200"
-					leaveFrom="opacity-100"
-					leaveTo="opacity-0"
-				>
-					<Dialog.Overlay />
-				</Transition.Child>
+		<Transition show={isOpen} as={Fragment} afterLeave={() => console.log("done")}>
+			<Dialog initialFocus={imageRef} onClose={() => setIsOpen(false)} >
+				<div className="fixed inset-0 z-10 overflow-y-auto">
+					<div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+						<Transition.Child
+							enter="ease-out duration-300"
+							enterFrom="opacity-0"
+							enterTo="opacity-100"
+							leave="ease-in duration-200"
+							leaveFrom="opacity-100"
+							leaveTo="opacity-0"
+						>
+							<Dialog.Overlay className="fixed inset-0 transition-opacity">
+								<div className="absolute inset-0 bg-black opacity-75"></div>
+							</Dialog.Overlay>
+						</Transition.Child>
 
-				{/*
-          ...and another Transition.Child to apply a separate transition
-          to the contents.
-        */}
-				<Transition.Child
-					enter="ease-out duration-300"
-					enterFrom="opacity-0 scale-95"
-					enterTo="opacity-100 scale-100"
-					leave="ease-in duration-200"
-					leaveFrom="opacity-100 scale-100"
-					leaveTo="opacity-0 scale-95"
-				>
-					<Dialog.Title>Deactivate account</Dialog.Title>
-					<button ref={imageRef}>Complete order</button>
-					{/* ... */}
-				</Transition.Child>
+						<Transition.Child
+							enter="ease-out transform duration-300"
+							enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+							enterTo="opacity-100 translate-y-0 sm:scale-100"
+							leave="ease-in transform duration-200"
+							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+						>
+							{/* This element is to trick the browser into centering the modal contents. */}
+							<span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+								&#8203;
+							</span>
+							<div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+								{/* <img  src={"/showcase/" + src} alt="" /> */}
+								hello
+                                <br />
+								<button ref={imageRef}>Exit </button>
+							</div>
+
+							{/* <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+								div
+							</div> */}
+						</Transition.Child>
+					</div>
+				</div>
 			</Dialog>
 		</Transition>
 	);
