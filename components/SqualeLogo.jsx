6ae27090e849453a1879c8f38d30712e2e55fd39
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-06-16 02:47:02 +0200

    making the website responsive

--- components/SqualeLogo.jsx
+++ components/SqualeLogo.jsx
@@ -3,8 +3,8 @@ import * as React from "react"
 function SvgComponent(props) {
   return (
     <svg
-      width={432}
-      height={70}
+      // width={432}
+      // height={70}
       viewBox="0 0 432 70"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,13 @@ export default function Home({ images }) {
       <div className="overflow-hidden">
         <div className="relative">
           <div className="mx-auto bg-white xl:max-w-screen-xl md:max-w-screen-md">
-            <div className="flex items-center h-full pt-32 pb-48">
-              <div className="w-2/3">
-                <h1 aria-label="Squale.Agency" className="relative">
-                  <img src="/SqualeLogo.png" className="absolute left-squale" alt="squale logo" />
-                  <SqualeLogo />
+            <div className="flex flex-col items-start h-full px-4 pt-8 pb-48 space-y-3 sm:pt-16 xl:pt-32 2xl:space-y-0 lg:px-0 2xl:items-center 2xl:flex-row">
+              <div className="2xl:w-2/3">
+                <h1 aria-label="Squale.Agency" className="relative flex items-center 2xl:items-stretch">
+                  <img src="/SqualeLogo.png" className="absolute hidden 2xl:block left-squale" alt="squale logo" />
+                  <SqualeLogo className="sm:h-[70px]" />
                   <span className="hidden">Squale.Agency</span>
+                  <img src="/SqualeLogo.png" className="block ml-2 h-14 sm:h-20 2xl:hidden img-rendering-good" alt="squale logo" />
                 </h1>
                 <span className="mt-6 text-2xl text-gray-900">
                   Agence de <b className="font-medium text-chrome-yellow">développement web</b> et <b className="font-medium text-teal-blue">infographie</b>
@@ -53,12 +54,12 @@ export default function Home({ images }) {
               </div>
             </div>
           </div>
-          <img className="absolute z-[1] right-leaves" src="/right-leaves.png" alt="leaves" />
+          <img className="absolute z-[1] hidden sm:block right-leaves" src="/right-leaves.png" alt="leaves" />
         </div>
         <div className="relative z-10 ">
           <div className="-mt-40">
             <div className="w-full h-8 bg-transparent"></div>
-            <div className="z-20 max-w-screen-md pt-8 pb-8 mx-auto xl:max-w-screen-xl filter drop-shadow-2xl masonry-2-col md:masonry-3-col gap-y-6 gap-x-2">
+            <div className="z-20 max-w-screen-md px-4 pt-8 pb-8 mx-auto xl:max-w-screen-xl filter drop-shadow-2xl sm:masonry-2-col sm:px-0 md:masonry-3-col gap-y-6 gap-x-2">
               {images.map((e, idx) => (
                 <ImageCard key={idx} src={e} setIsOpen={openModal} setImage={setImage} />
               ))}
